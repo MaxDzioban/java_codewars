329471747d8a33b186567c70068e22134238a1a8
MaxDz <dzoban.pn@ucu.edu.ua>
2024-07-22 11:39:09 +0300

    Needle

---
+++ Needle.java
@@ -0,0 +1,10 @@
+public class Needle {
+    public static String findNeedle(Object[] haystack) {
+        for (int i = 0; i < haystack.length; i++) {
+            if ("needle".equals(haystack[i])) {
+                return "found the needle at position " + i;
+            }
+        }
+        return "needle not found";
+    }
+}
